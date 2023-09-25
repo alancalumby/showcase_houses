dd1b9e9aea71700cfe00e8d6fbef602499fe5997
alancalumby <alancalumby@gmail.com>
2023-09-25 10:52:13 -0400

    Integrate Angular forms

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -7,7 +7,7 @@ import { HousingLocation } from './housinglocation';
 export class HousingService {
 
   readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
-  
+
   housingLocationList: HousingLocation[] = [
     {
       id: 0,
@@ -117,6 +117,10 @@ export class HousingService {
   
   getHousingLocationById(id: number): HousingLocation | undefined {
     return this.housingLocationList.find(housingLocation => housingLocation.id === id);
+  }
+
+  submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(`Homes application received: firstName: ${firstName}, lastName: ${lastName}, email: ${email}.`);
   }
 
   constructor() { }
